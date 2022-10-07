8a1ca6ba615c17c7a9c41de5e3c71594de2c144e
ddm479 <ehdtjq5247@gmail.com>
2022-10-08 02:41:38 +0900

    Merge branch 'master' of https://github.com/ddm479/project

--- src/component/page/LoginGoogle.js
+++ src/component/page/LoginGoogle.js
@@ -1,8 +1,71 @@
 import React from 'react';
+<<<<<<< Updated upstream
+=======
+// import GoogleLogin from 'react-google-login'; // 다른 패키지로 사용
+import styled from "styled-components";
+import { useNavigate } from 'react-router-dom';
+import jwt_decode from "jwt-decode";
+import axios from "axios";
+
+// https://www.npmjs.com/package/@react-oauth/google 
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
+import { useGoogleLogin } from '@react-oauth/google';
+
+// axios.defaults.withCredentials = true;
+
+function LoginGoogle() {
+  const clientId = "1037417891725-d7fnfaa8up490p8ghd6cl6tmc9nbbi4v.apps.googleusercontent.com"; // 로그인을 한 상태에서 하면 구글 로그인창이 안뜸
+  // 구글 oauth 클라이언트 id
+  const navigate = useNavigate();
+
+  const login = useGoogleLogin({
+    onSuccess: tokenResponse => console.log(tokenResponse),
+  });
+
+  return (
+    <GoogleOAuthProvider clientId={clientId}>
+      <GoogleLogin
+        onSuccess={(credentialResponse) => {
+          console.log("credentialResponse.credential입니다 ", credentialResponse.credential);
+          var decoded = jwt_decode(credentialResponse.credential);
+          console.log("jwt_decode(credentialResponse.credential)의 내용입니다", decoded);
+          console.log("test", decoded.name, decoded.email);
+          
+           // package.json에 proxy 설정하면 한가지 origin으로부터의 cors처리만 가능
+           // https관련 추가문제 발생? 
+          axios.post("/register", 
+          // https로 하면 error남 http로 (net::ERR_SSL_PROTOCOL_ERROR) 해결
+          {
+            email: decoded.email,
+            nickname: decoded.name,
+            email: "testtest",
+            // withCredentials: true, // 쿠키 cors 통신 설정
+          },
+          )
+            .then(function (response) {
+              console.log(response);
+              console.log(JSON.stringify(response));
+            }).catch(function (error) {
+              console.log(error);
+              console.log(error.response);
+            })
+        }}
+        onError={() => {
+          console.log('Login Failed');
+        }} />
+    </GoogleOAuthProvider>
+  )
+}
+
+export default LoginGoogle;
+
+/* import React from 'react';
+>>>>>>> Stashed changes
 // import GoogleLogin from 'react-google-login';
 import styled from "styled-components";
 import { useNavigate } from 'react-router-dom';
-const Wrapper = styled.div`
+/* const Wrapper = styled.div`
     paddign: 1em;
     background: grey
     display: flex;
@@ -42,8 +105,8 @@ function LoginGoogle() {
         onFailure={onFailure}
         cookiePolicy="single_host_origin"
       />
-    </div> */}
+    </div> }
   )
 }
 
-export default LoginGoogle;
+export default LoginGoogle; */
