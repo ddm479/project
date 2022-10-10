089d87f4a70204e649ca51223edf1be65a65798c
ddm479 <ehdtjq5247@gmail.com>
2022-10-11 00:18:14 +0900

    g
    g

--- src/component/page/LoginGoogle.js
+++ src/component/page/LoginGoogle.js
@@ -1,13 +1,13 @@
 import React from 'react';
 // import GoogleLogin from 'react-google-login'; // 다른 패키지로 사용
-import styled from "styled-components";
+// import styled from "styled-components";
 import { useNavigate } from 'react-router-dom';
-import jwt_decode from "jwt-decode";
+// import jwt_decode from "jwt-decode";
 import axios from "axios";
 
 // https://www.npmjs.com/package/@react-oauth/google 
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import { GoogleLogin } from '@react-oauth/google';
+// import { GoogleLogin } from '@react-oauth/google';
 import { useGoogleLogin } from '@react-oauth/google';
 import { OAuth2Client } from 'google-auth-library';
 
