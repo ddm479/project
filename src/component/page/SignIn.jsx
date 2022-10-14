9db30faefec805ec5873d753bb7c9e28d3fba471
ddm479 <ehdtjq5247@gmail.com>
2022-10-15 01:45:56 +0900

    Update SignIn.jsx

--- src/component/page/SignIn.jsx
+++ src/component/page/SignIn.jsx
@@ -15,27 +15,29 @@ import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import GoogleLogin from "./GoogleLogin";
 import LoginGoogle from "./LoginGoogle";
+import styled from "styled-components";
 // import GoogleLogin from "react-google-login";
-function Copyright(props) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
 
-const theme = createTheme();
 
+const Wrapper = styled.div`
+    paddign: 1em;
+    background: grey;
+    margin: 5vh 35vw;  
+    text-align: center;
+    border: 0.2rem solid black;
+
+    display: flex;
+    flex-direction: column; 
+    align-items: center;
+    justify-content: flex-start;
+`;
+// 반응형 웹 고민중
+const Title = styled.div`
+  padding-top: 1rem;
+  font-size: 5em;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica
+      Neue, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji;
+`
 function SignIn() {
   const navigate = useNavigate();
   const handleSubmit = (event) => {
@@ -48,86 +50,25 @@ function SignIn() {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box // div 같은 느낌
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar
-            sx={{
-              m: 1,
-              bgcolor: "secondary.main",
-            }} /* 구글 우상단에 있는 profile 사진같은 css 효과를 줄 수 있는 컴포넌트*/
-          >
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography
-            component="h1"
-            variant="h5" /* h1 태그로 감싼다 디자인은 h5 태그, 텍스트 설정을 해줄 수 있는 컴포넌트 */
-          >
-            Sign in
-          </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
-          >
-            <TextField
-              margin="normal"
-              required // 필수 입력
-              fullWidth
-              id="email"
-              label="Email Address" // 보이는 내용
-              name="email"
-              autoComplete="email"
-              autoFocus // 처음에 자동으로 커서 이동
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained" // 모양 결정, 색이꽉찬
-              sx={{ mt: 3, mb: 2 }} // 간단한 css, margintop, bottom
-            >
-              Sign In
-            </Button>
-            <LoginGoogle/>
-            <Grid container /* container가 있는게 행*/>
-              
-              <Grid item>
-                <Link
-                  href="/Agree"
-                  variant="body2"
-                  /* onClick={() => {
-                    navigate("/");
-                  }} */
-                >
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        
-      </Container>
-    </ThemeProvider>
+    <Wrapper>
+      <Title>Bitwise</Title>
+      <LoginGoogle />
+      <Link
+        href="/Agree"
+        variant="body2"
+      /* onClick={() => {
+        navigate("/");
+      }} */
+      >
+        {"Don't have an account? Sign Up"}
+      </Link>
+
+
+
+    </Wrapper>
+
+
+
   );
 }
 
