dc8cd40998cad4b810c057244e71b571257e1ec3
ddm479 <ehdtjq5247@gmail.com>
2022-10-14 21:01:22 +0900

    GoogleOAuthProvider
    App 컴포넌트를 GoogleOAuthProvider 컴포넌트로 감싸줘야 출력됨

--- src/index.js
+++ src/index.js
@@ -3,21 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
-
-
+const clientId = "1037417891725-d7fnfaa8up490p8ghd6cl6tmc9nbbi4v.apps.googleusercontent.com"; // 로그인을 한 상태에서 하면 구글 로그인창이 안뜸
+// 구글 oauth 클라이언트 id
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={clientId}>
+      <App />
+    </GoogleOAuthProvider> 
   </React.StrictMode>
 
 
 );
 
+// <App> 컴포넌트를 감싸줘야함
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
