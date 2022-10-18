93037997d9db4013612ea205c9e73eb7740dca0d
ddm479 <ehdtjq5247@gmail.com>
2022-10-19 01:34:06 +0900

    delete node_modules
    node_modules 와 package-lock.json 삭제 후 npm install --force
    module not found: error: cannot resolve

--- src/pages/login/PrivacyPolicy.jsx
+++ src/pages/login/PrivacyPolicy.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import Content from "../Content";
-import  Title  from "../Title";
+import Content from "../../components/Content";
+import Title from '../../components/Title';
 // em은 해당 단위가 사용되고 있는 요소의 font-size 속성 값이 기준으로 비례함
 // rem은 최상위 요소(root)를 기준, HTML에서는 <html>요소의 font-size 속성 값이 기준으로 비례 
 const Wrapper = styled.div`
