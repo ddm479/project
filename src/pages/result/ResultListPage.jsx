29a3ce98f28dd0669774fa38adcd1b16083483d7
ddm479 <ehdtjq5247@gmail.com>
2022-10-29 00:34:10 +0900

    ResultListPage
    ResultListPage

--- src/pages/result/ResultListPage.jsx
+++ src/pages/result/ResultListPage.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Typography } from '@mui/material';
+import axios from 'axios';
 import ResultListView from '../../components/view/ResultView';
 import sampleImageContents from "../../jsonDataset/sampleImageContents";
 import sampleResults from '../../jsonDataset/sampleResults.json';
@@ -8,17 +9,42 @@ import sampleResults from '../../jsonDataset/sampleResults.json';
 const Wrapper = styled.div``;
 
 function ResultListPage() {
-    const results = sampleResults.map((result) => ({
-        ...result,
-        imageUrl: sampleImageContents[Number(result.imageId)].url,
-        imageAlt: sampleImageContents[Number(result.imageId)].alt,
-    }));
+    const [requests, setRequests] = useState([]);
+    useEffect(() => {
+        axios
+            .post('https://bitwise.ljlee37.com:8080/requestList', {
+                user_id: 'test',
+            })
+            .then((response) => {
+                const out = response.data.queryResult.map((request) => ({
+                    resultId: request.request_id,
+                    imageUrl: request.path,
+                    imageAlt: decodeURIComponent(request.name),
+                    inference_status: request.status,
+                    class_category: request.request_result,
+                    date: request.upload_date_time,
+                }));
+                setRequests(() => out);
+            })
+            .catch(() => {
+                const results = sampleResults.map((result) => ({
+                    ...result,
+                    imageUrl: sampleImageContents[Number(result.imageId)].url,
+                    imageAlt: sampleImageContents[Number(result.imageId)].alt,
+                }));
+                setRequests(() => results);
+            });
+    }, []);
     return (
         <Wrapper>
             <Typography variant="h5" gutterBottom>
                 결과 목록 페이지
             </Typography>
-            <ResultListView results={results} />
+            <ResultListView
+                results={requests.sort(
+                    (a, b) => Date.parse(b.date) - Date.parse(a.date),
+                )}
+            />
         </Wrapper>
     );
 }
