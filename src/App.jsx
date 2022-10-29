c51065a331e99ff743ab38b18455421d31371598
ddm479 <ehdtjq5247@gmail.com>
2022-10-29 18:08:18 +0900

    scripts "start", "build"
    다름 주의, 리액트 .env사용법 이상함

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
   });
   return (
     // <BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL}>
-    <BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <ThemeProvider theme={theme}>
 
         <Routes>
